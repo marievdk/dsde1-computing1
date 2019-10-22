d1aedc8d20214b47fafad2d36c7f295632dae817
marievdk <marie.van-der-klink19@imperial.ac.uk>
2019-10-22 12:05:51 +0100

    Still have to figure negative values

--- Week-3/pendulum.py
+++ Week-3/pendulum.py
@@ -1,10 +1,17 @@
 # PENDULUM FORMULA !
-L = (input('What is the length of your pendulum ? '))
-g = (input('Which value of g do you want me to input ? '))
+L = int((input('What is the length of your pendulum ? ')))
+g = int((input('Which value of g do you want me to input ? ')))
 
 import math
 
 def period(L, g):
-    return 2*math.pi*((L/g)**(1/2))
+    try:
+        return 2*(math.pi)*((L/g)**(1/2))
+    except TypeError:
+        print('Your input has to be numerical.')
+    except ValueError:
+        print('The value that you have entered breaks this function.')
+    except ZeroDivisionError:
+        print('You can not divide by zero.')
 
 print(period(L, g))
